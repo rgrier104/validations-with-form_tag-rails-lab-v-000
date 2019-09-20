7a4ed44d08116467c024db179e59d93d8b8054f5
Rachel Grier <rgrier104@gmail.com>
2019-09-20 16:24:43 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :email, uniqueness: true
-  validtes :phone_number, length: { is: 10 }
+  validates :phone_number, length: { is: 10 }
 end
